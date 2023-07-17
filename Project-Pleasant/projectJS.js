8a449e78db3ee853d126fceded521dcca7484792
Joseph Pleasant <jbpleasant@my.waketech.edu>
2023-07-17 19:04:55 -0400

    Add files via upload

--- Project-Pleasant/projectJS.js
+++ Project-Pleasant/projectJS.js
@@ -1,91 +1,173 @@
-document.getElementById("generatePlanButton").addEventListener("click", function() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var goal = document.getElementById("goal").value;
-  
-    var mondayBreakfast = document.getElementById("mondayBreakfast").value;
-    var mondaySnack = document.getElementById("mondaySnack").value;
-    var mondayLunch = document.getElementById("mondayLunch").value;
-    var mondayDinner = document.getElementById("mondayDinner").value;
-
-    var mondayBreakfast = document.getElementById("tuesdayBreakfast").value;
-    var mondaySnack = document.getElementById("tuesdaySnack").value;
-    var mondayLunch = document.getElementById("tuesdayLunch").value;
-    var mondayDinner = document.getElementById("tuesdayDinner").value;
-
-    var mondayBreakfast = document.getElementById("wednesdayBreakfast").value;
-    var mondaySnack = document.getElementById("wednesdaySnack").value;
-    var mondayLunch = document.getElementById("wednesdayLunch").value;
-    var mondayDinner = document.getElementById("wednesdayDinner").value;
-
-    var mondayBreakfast = document.getElementById("thursdayBreakfast").value;
-    var mondaySnack = document.getElementById("thursdaySnack").value;
-    var mondayLunch = document.getElementById("thursdayLunch").value;
-    var mondayDinner = document.getElementById("thursdayDinner").value;
-
-    var mondayBreakfast = document.getElementById("fridayBreakfast").value;
-    var mondaySnack = document.getElementById("fridaySnack").value;
-    var mondayLunch = document.getElementById("fridayLunch").value;
-    var mondayDinner = document.getElementById("fridayDinner").value;
-
-    var mondayBreakfast = document.getElementById("saturdayBreakfast").value;
-    var mondaySnack = document.getElementById("saturdaySnack").value;
-    var mondayLunch = document.getElementById("saturdayLunch").value;
-    var mondayDinner = document.getElementById("saturdayDinner").value;
-
-    var mondayBreakfast = document.getElementById("sundayBreakfast").value;
-    var mondaySnack = document.getElementById("sundaySnack").value;
-    var mondayLunch = document.getElementById("sundayLunch").value;
-    var mondayDinner = document.getElementById("sundayDinner").value;
+// JavaScript code for generating the meal plan
+document.getElementById('generatePlanButton').addEventListener('click', function() {
     
-    // New Meal Plan Window
-    var content = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Meal Plan for ${name}</title>
-        <style>
-            /* Add your desired CSS styling here */
-        </style>
-        </head>
-        <body>
-        <h1>Meal Plan for ${name}</h1>
-        
-        <h2>Email: ${email}</h2>
-        <h2>Goal for the Week:</h2>
-        <p>${goal}</p>
-        
-        <h2>Meal Plan:</h2>
-        
-        <h3>Monday:</h3>
-        <p>Breakfast: ${mondayBreakfast}</p>
-        <p>Snack: ${mondaySnack}</p>
-        <p>Lunch: ${mondayLunch}</p>
-        <p>Dinner: ${mondayDinner}</p>
-        
-        <!-- Repeat the above section for the remaining days of the week -->
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var goal = document.getElementById('goal').value;
 
-        <button onclick="window.print()">Print</button>
-        <button onclick="downloadMealPlan()">Download</button>
-
-        <script>
-            function downloadMealPlan() {
-            var content = document.documentElement.outerHTML;
-            var blob = new Blob([content], { type: "text/html" });
-            var url = URL.createObjectURL(blob);
-            var a = document.createElement("a");
-            a.href = url;
-            a.download = "meal_plan.html";
-            a.click();
-            URL.revokeObjectURL(url);
-            }
-        </script>
-        </body>
-        </html>
+    
+    var requiredFields = [name, email, goal];
+    for (var i = 0; i < requiredFields.length; i++) {
+    if (requiredFields[i].trim() === '') {
+      alert('Please complete all required fields.');
+      return;
+  }
+}
+    
+    // Generate the meal plan HTML
+    var mealPlanHTML = `
+      <h1>Meal Plan for ${name}</h1>
+      <h2>Email: ${email}</h2>
+      <h3>Weekly Goal: ${goal}</h3>
+      <h4>Meal Plan:</h4>
+      <div class="container">
+        <div class="day day-monday">
+          <h2>Monday:</h2>
+          <p>Breakfast: ${document.getElementById('mondayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('mondaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('mondayLunch').value}</p>
+          <p>Snack: ${document.getElementById('mondaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('mondayDinner').value}</p>
+        </div>
+        <div class="day day-tuesday">
+          <h2>Tuesday:</h2>
+          <p>Breakfast: ${document.getElementById('tuesdayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('tuesdaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('tuesdayLunch').value}</p>
+          <p>Snack: ${document.getElementById('tuesdaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('tuesdayDinner').value}</p>
+        </div>
+        <div class="day day-wednesday">
+          <h2>Wednesday:</h2>
+          <p>Breakfast: ${document.getElementById('wednesdayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('wednesdaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('wednesdayLunch').value}</p>
+          <p>Snack: ${document.getElementById('wednesdaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('wednesdayDinner').value}</p>
+        </div>
+        <div class="day day-thursday">
+          <h2>Thursday:</h2>
+          <p>Breakfast: ${document.getElementById('thursdayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('thursdaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('thursdayLunch').value}</p>
+          <p>Snack: ${document.getElementById('thursdaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('thursdayDinner').value}</p>
+        </div>
+        <div class="day day-friday">
+          <h2>Friday:</h2>
+          <p>Breakfast: ${document.getElementById('fridayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('fridaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('fridayLunch').value}</p>
+          <p>Snack: ${document.getElementById('fridaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('fridayDinner').value}</p>
+        </div>
+        <div class="day day-saturday">
+          <h2>Saturday:</h2>
+          <p>Breakfast: ${document.getElementById('saturdayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('saturdaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('saturdayLunch').value}</p>
+          <p>Snack: ${document.getElementById('saturdaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('saturdayDinner').value}</p>
+        </div>
+        <div class="day day-sunday">
+          <h2>Sunday:</h2>
+          <p>Breakfast: ${document.getElementById('sundayBreakfast').value}</p>
+          <p>Snack: ${document.getElementById('sundaySnack').value}</p>
+          <p>Lunch: ${document.getElementById('sundayLunch').value}</p>
+          <p>Snack: ${document.getElementById('sundaySnack2').value}</p>
+          <p>Dinner: ${document.getElementById('sundayDinner').value}</p>
+        </div>
+      </div>
     `;
-
-    var newWindow = window.open();
-    newWindow.document.open();
-    newWindow.document.write(content);
+    
+    // Open a new window with the generated meal plan
+    var newWindow = window.open('', '_blank');
+    newWindow.document.write(mealPlanHTML);
     newWindow.document.close();
-    });
+    
+    // Add print button to the generated meal plan
+    var printButtonHTML = `
+      <button onclick="window.print()">Print</button>
+    `;
+    newWindow.document.body.insertAdjacentHTML('beforeend', printButtonHTML);
+    
+    // Add download button to the generated meal plan
+    var downloadButtonHTML = `
+      <button onclick="downloadMealPlan()">Download</button>
+    `;
+    newWindow.document.body.insertAdjacentHTML('beforeend', downloadButtonHTML);
+  });
+  
+  // Function to download the meal plan as a text file
+  function downloadMealPlan() {
+    var name = document.getElementById('name').value;
+    var mealPlanText = `
+      Meal Plan for ${name}
+  
+      Email: ${document.getElementById('email').value}
+      Weekly Goal: ${document.getElementById('goal').value}
+  
+      Monday:
+      Breakfast: ${document.getElementById('mondayBreakfast').value}
+      Snack: ${document.getElementById('mondaySnack').value}
+      Lunch: ${document.getElementById('mondayLunch').value}
+      Snack: ${document.getElementById('mondaySnack2').value}
+      Dinner: ${document.getElementById('mondayDinner').value}
+  
+      Tuesday:
+      Breakfast: ${document.getElementById('tuesdayBreakfast').value}
+      Snack: ${document.getElementById('tuesdaySnack').value}
+      Lunch: ${document.getElementById('tuesdayLunch').value}
+      Snack: ${document.getElementById('tuesdaySnack2').value}
+      Dinner: ${document.getElementById('tuesdayDinner').value}
+  
+      Wednesday:
+      Breakfast: ${document.getElementById('wednesdayBreakfast').value}
+      Snack: ${document.getElementById('wednesdaySnack').value}
+      Lunch: ${document.getElementById('wednesdayLunch').value}
+      Snack: ${document.getElementById('wednesdaySnack2').value}
+      Dinner: ${document.getElementById('wednesdayDinner').value}
+  
+      Thursday:
+      Breakfast: ${document.getElementById('thursdayBreakfast').value}
+      Snack: ${document.getElementById('thursdaySnack').value}
+      Lunch: ${document.getElementById('thursdayLunch').value}
+      Snack: ${document.getElementById('thursdaySnack2').value}
+      Dinner: ${document.getElementById('thursdayDinner').value}
+  
+      Friday:
+      Breakfast: ${document.getElementById('fridayBreakfast').value}
+      Snack: ${document.getElementById('fridaySnack').value}
+      Lunch: ${document.getElementById('fridayLunch').value}
+      Snack: ${document.getElementById('fridaySnack2').value}
+      Dinner: ${document.getElementById('fridayDinner').value}
+  
+      Saturday:
+      Breakfast: ${document.getElementById('saturdayBreakfast').value}
+      Snack: ${document.getElementById('saturdaySnack').value}
+      Lunch: ${document.getElementById('saturdayLunch').value}
+      Snack: ${document.getElementById('saturdaySnack2').value}
+      Dinner: ${document.getElementById('saturdayDinner').value}
+  
+      Sunday:
+      Breakfast: ${document.getElementById('sundayBreakfast').value}
+      Snack: ${document.getElementById('sundaySnack').value}
+      Lunch: ${document.getElementById('sundayLunch').value}
+      Snack: ${document.getElementById('sundaySnack2').value}
+      Dinner: ${document.getElementById('sundayDinner').value}
+    `;
+  
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(mealPlanText));
+    element.setAttribute('download', 'meal_plan.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+  
+  // Clear the form
+  document.getElementById('clearButton').addEventListener('click', function() {
+    document.getElementById('mealPlanForm').reset();
+  });
+  
